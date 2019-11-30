2e8dcd10b8724daecc006a7258977e2d5200ad5c
Mogens Brødsgaard Lund <salza@salza.dk>
2019-11-30 09:51:41 +0100

    Removed invalidated test

--- test/liq2/modes/fundamental_mode_test.clj
+++ test/liq2/modes/fundamental_mode_test.clj
@@ -9,8 +9,3 @@
     (is (= (get-namespace (buffer/buffer "(ns abc.def)\n  asdf")) "abc.def"))
     (is (= (get-namespace (buffer/buffer "(ns abc.def\n  asdf)")) "abc.def"))))
 
-(deftest eval-sexp-at-point-test
-  (testing "Eval sexp at point"
-    (let [buf (-> (buffer/buffer "(ns abc.def)\n(+ 1 2 3)")
-                  (buffer/set-point 2 3))]
-      (is (= (eval-sexp-at-point buf) 6)))))
